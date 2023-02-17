a53b4b67f7023bf0bab2180ab44b78f262bbbe93
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-02-17 22:45:46 +0200

    Update

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { useFont } from "./hooks/useFont";
 import RegistrationScreen from "./Screens/auth/RegistrationScreen";
 import LoginScreen from "./Screens/auth/LoginScreen";
+import Home from "./Screens/main/Home";
+import PostsScreen from "./Screens/main/PostsScreen";
+import CreatePostsScreen from "./Screens/main/CreatePostsScreen";
+import ProfileScreen from "./Screens/main/ProfileScreen";
 
 export default function App() {
   const { appIsReady, onLayoutRootView } = useFont();
-  const Stack = createStackNavigator();
+  const AuthStack = createStackNavigator();
+  const MainTab = createBottomTabNavigator();
 
   if (!appIsReady) {
     return null;
@@ -15,18 +21,29 @@ export default function App() {
 
   return (
     <NavigationContainer onLayout={onLayoutRootView}>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Registration"
-          component={RegistrationScreen}
+      <MainTab.Navigator>
+        <MainTab.Screen name="PostsScreen" component={PostsScreen} />
+        <MainTab.Screen
+          name="CreatePostsScreen"
+          component={CreatePostsScreen}
         />
-        <Stack.Screen
-          options={{ headerShown: false }}
-          name="Login"
-          component={LoginScreen}
-        />
-      </Stack.Navigator>
+        <MainTab.Screen name="ProfileScreen" component={ProfileScreen} />
+      </MainTab.Navigator>
     </NavigationContainer>
   );
+}
+
+{
+  /* <AuthStack.Navigator>
+  <AuthStack.Screen
+    options={{ headerShown: false }}
+    name="Registration"
+    component={RegistrationScreen}
+  />
+  <AuthStack.Screen
+    options={{ headerShown: false }}
+    name="Login"
+    component={LoginScreen}
+  />
+</AuthStack.Navigator>; */
 }
