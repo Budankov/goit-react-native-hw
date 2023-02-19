73cb00e337971a23d915b5fba8d90641ca237b6f
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-02-19 13:58:32 +0200

    Update

--- Screens/main/CreatePostsScreen.js
+++ Screens/main/CreatePostsScreen.js
@@ -1,10 +1,32 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { Camera } from "expo-camera";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const CreatePostsScreen = () => {
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState("");
+
+  const takePhoto = async () => {
+    const photo = await camera.takePictureAsync();
+    setPhoto(photo.uri);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>CreatePostsScreen</Text>
+      <Camera style={styles.camera} ref={setCamera}>
+        {photo && (
+          <View style={styles.takePhotoContainer}>
+            <Image
+              style={styles.takePhotoImage}
+              source={{ uri: photo }}
+            ></Image>
+          </View>
+        )}
+        <TouchableOpacity style={styles.snapContainer} onPress={takePhoto}>
+          <Text style={styles.snap}>SNAP</Text>
+        </TouchableOpacity>
+      </Camera>
     </View>
   );
 };
@@ -12,8 +34,36 @@ const CreatePostsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  camera: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  snap: {
+    color: "#FFF",
+  },
+  snapContainer: {
+    borderWidth: 1,
+    borderRadius: 50,
+    borderColor: "#FF0000",
+    width: 70,
+    height: 70,
+    marginTop: 200,
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 20,
+  },
+  takePhotoContainer: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    borderColor: "#FFF",
+    borderWidth: 1,
+  },
+  takePhotoImage: {
+    width: 200,
+    height: 200,
   },
 });
 
